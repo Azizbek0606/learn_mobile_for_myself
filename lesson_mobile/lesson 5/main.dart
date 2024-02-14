3a70f80e6c4b28ef08eb2a9ce4cc7eef4a0dd0e7
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-02-14 11:54:00 +0500

    create main.dart

---
+++ lesson_mobile/lesson 5/main.dart
@@ -0,0 +1,7 @@
+// private qilisdan asosiy maqsad logikani buzilib ketshini oldini oladi / mantiqni himoyalaydi
+void main() {
+  // inkapsulatsiy
+  // class ichidagi malumotlar har gal public holatda bo'ladi
+  // private qilish uchun pastgi chiziqcha ishlatiladi
+  
+}
