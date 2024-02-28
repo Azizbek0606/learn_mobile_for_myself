eff5663d7f354165314aa41fc9892ddedefb6b99
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-02-28 08:58:35 +0500

    update

--- homework_mobile/15 tasks/main.dart
+++ homework_mobile/15 tasks/main.dart
@@ -478,40 +478,40 @@
 
 // task 15
 
-// import 'dart:io';
+import 'dart:io';
 
-// class Personddress {
-//   String _name;
-//   String _street;
-//   int _postal_code;
-//   Personddress(this._name, this._street, this._postal_code);
-//   set set_name(new_name) {
-//     _name = new_name;
-//   }
+class Personddress {
+  String _name;
+  String _street;
+  int _postal_code;
+  Personddress(this._name, this._street, this._postal_code);
+  set set_name(new_name) {
+    _name = new_name;
+  }
 
-//   set set_street(new_street) {
-//     _street = new_street;
-//   }
+  set set_street(new_street) {
+    _street = new_street;
+  }
 
-//   set set_code(new_code) {
-//     _postal_code = new_code;
-//   }
+  set set_code(new_code) {
+    _postal_code = new_code;
+  }
 
-//   get get_name => this._name;
-//   get get_street => this._street;
-//   get get_postal_code => this._postal_code;
-// }
+  get get_name => this._name;
+  get get_street => this._street;
+  get get_postal_code => this._postal_code;
+}
 
-// void main() {
-//   print("ism kiriting");
-//   String name = stdin.readLineSync()!;
-//   print("manzlni kiriting");
-//   String street = stdin.readLineSync()!;
-//   print("pochta indexini kiriting");
-//   int postal_index = int.parse(stdin.readLineSync()!);
-//   Personddress new_addres = Personddress(name, street, postal_index);
-//   print("ism: ${new_addres.get_name}");
-//   print("manzil ${new_addres.get_street}");
-//   print("pochta index: ${new_addres.get_postal_code}");
-//   new_addres._postal_code = 12;
-// }
+void main() {
+  print("ism kiriting");
+  String name = stdin.readLineSync()!;
+  print("manzlni kiriting");
+  String street = stdin.readLineSync()!;
+  print("pochta indexini kiriting");
+  int postal_index = int.parse(stdin.readLineSync()!);
+  Personddress new_addres = Personddress(name, street, postal_index);
+  print("ism: ${new_addres.get_name}");
+  print("manzil ${new_addres.get_street}");
+  print("pochta index: ${new_addres.get_postal_code}");
+  new_addres._postal_code = 12;
+}
--- homework_mobile/polimarfizm/main.dart
+++ homework_mobile/polimarfizm/main.dart
@@ -1,47 +1,77 @@
 // task 1
 
-import 'dart:io';
+// import 'dart:io';
 
-class Shape {
-  double calculateArea(){
-    return 0;
-  }
-}
+// class Shape {
+//   double calculateArea(){
+//     return 0;
+//   }
+// }
 
-class Circle extends Shape {
-  double radius;
+// class Circle extends Shape {
+//   double radius;
 
-  Circle(this.radius);
+//   Circle(this.radius);
 
-  @override
-  double calculateArea() {
-    return 3.14159 * radius * radius;
-  }
-}
+//   @override
+//   double calculateArea() {
+//     return 3.14159 * radius * radius;
+//   }
+// }
 
-class Rectangle extends Shape {
-  double width;
-  double height;
+// class Rectangle extends Shape {
+//   double width;
+//   double height;
 
-  Rectangle(this.width, this.height);
+//   Rectangle(this.width, this.height);
 
-  @override
-  double calculateArea() {
-    return width * height;
-  }
-}
+//   @override
+//   double calculateArea() {
+//     return width * height;
+//   }
+// }
 
-void main() {
-  print("Doira radiusi: ");
-  double circle_num = double.parse(stdin.readLineSync()!);
-  Circle new_circle = Circle(circle_num);
-  print("doiraning yuzi: ${new_circle.calculateArea()}");
+// void main() {
+//   print("Doira radiusi: ");
+//   double circle_num = double.parse(stdin.readLineSync()!);
+//   Circle new_circle = Circle(circle_num);
+//   print("doiraning yuzi: ${new_circle.calculateArea()}");
+
+//   print("to'rtburchak eni: ");
+//   double width_rec = double.parse(stdin.readLineSync()!);
+//   print("to'rtburchak yuzi: ");
+//   double heigth_rec = double.parse(stdin.readLineSync()!);
+//   Rectangle new_rectngle = Rectangle(width_rec, heigth_rec);
+//   print("to'rtburchak yuzi: ${new_rectngle.calculateArea()}");
+// }
 
 
-  print("to'rtburchak eni: ");
-  double width_rec = double.parse(stdin.readLineSync()!);
-  print("to'rtburchak yuzi: ");
-  double heigth_rec = double.parse(stdin.readLineSync()!);
-  Rectangle new_rectngle = Rectangle(width_rec, heigth_rec);
-  print("to'rtburchak yuzi: ${new_rectngle.calculateArea()}");
-}
+// task 2
+
+// import 'dart:io';
+
+// class MainClss {
+//   void get_area() {}
+// }
+
+// class RectangleArea extends MainClss {
+//   int width;
+//   int height;
+//   RectangleArea(this.width, this.height);
+//   @override
+//   void get_area() {
+//     print("to'rtburchak perimetri: ${(width + height) * 2}");
+//   }
+// }
+
+// void main() {
+//   print("to'rtburchak eni: ");
+//   int width = int.parse(stdin.readLineSync()!);
+//   print("to'rtburchak bo'yi: ");
+//   int height = int.parse(stdin.readLineSync()!);
+//   RectangleArea first_rectangl = RectangleArea(width, height);
+//   first_rectangl.get_area();
+// }
+
+// task 3
+
