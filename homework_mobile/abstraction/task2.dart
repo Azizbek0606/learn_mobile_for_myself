a4bf1347aa452f5e2c1fa9b815ac8cc0be0656b3
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-03-01 18:49:11 +0500

    update

---
+++ homework_mobile/abstraction/task2.dart
@@ -0,0 +1,18 @@
+mixin LoggerMixin {
+  void log_message() {
+    print("Birinchi xabar");
+  }
+}
+
+class FileLogger with LoggerMixin {
+  void second_log_message() {
+    print("faylga malumot biriktirildi");
+  }
+}
+
+void main() {
+  FileLogger get_message = FileLogger();
+  get_message.log_message();
+  get_message.second_log_message();
+}
+   
