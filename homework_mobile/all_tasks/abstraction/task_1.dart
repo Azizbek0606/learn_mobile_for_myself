25fdd0eed1d21c5bd988db400a1be3bc92b4718f
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-03-05 15:18:43 +0500

    update

--- homework_mobile/all_tasks/abstraction/task_1.dart
+++ homework_mobile/all_tasks/abstraction/task_1.dart
@@ -1,1 +1,17 @@
-//task: 1
+mixin LoggerMixin {
+  void logMessage() {
+    print("Xabar");
+  }
+}
+
+class LogMessageClass with LoggerMixin {
+  void main_func() {
+    print("ikkinchi xabar");
+  }
+}
+
+void main() {
+  LogMessageClass new_message = LogMessageClass();
+  new_message.logMessage();
+  new_message.main_func();
+}
