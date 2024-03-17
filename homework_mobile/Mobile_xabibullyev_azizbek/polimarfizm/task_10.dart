39d66399fe71ec14bf41aafc80bcfe6209fdd2af
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-03-17 13:59:17 +0500

    update

--- homework_mobile/Mobile_xabibullyev_azizbek/polimarfizm/task_10.dart
+++ homework_mobile/Mobile_xabibullyev_azizbek/polimarfizm/task_10.dart
@@ -1,64 +1,25 @@
 class BankAccount {
-  late int balance;
+  void save_account() {}
 
-  BankAccount({int balance = 0}) {
-    this.balance = balance;
-  }
+  void change_account() {}
+}
 
-  void deposit(int amount) {
-    balance += amount;
-    print('$amount summa qo\'shildi. Jami hisobingiz: $balance summa');
-  }
-
-  void withdraw(int amount) {
-    if (balance >= amount) {
-      balance -= amount;
-      print('$amount summa ajratildi. Jami hisobingiz: $balance summa');
-    } else {
-      print('Noto\'g\'ri amal! Hisobingiz yetarli emas.');
-    }
+class SaveAccount extends BankAccount {
+  String username;
+  String password;
+  SaveAccount(this.username, this.password);
+  @override
+  void save_account() {
+    print("saqlandi");
   }
 }
 
-class SavingsAccount extends BankAccount {
-  SavingsAccount({int balance = 0}) : super(balance: balance);
-
+class ChangeAccount extends BankAccount {
+  String new_username;
+  String new_password;
+  ChangeAccount(this.new_username, this.new_password);
   @override
-  void deposit(int amount) {
-    super.deposit(amount + (amount * 0.02).toInt());
+  void change_account() {
+    print("o'zgartirildi");
   }
-
-  @override
-  void withdraw(int amount) {
-    if (amount > balance) {
-      print('Noto\'g\'ri amal! Hisobingiz yetarli emas.');
-    } else {
-      super.withdraw(amount);
-    }
-  }
-}
-
-class CheckingAccount extends BankAccount {
-  CheckingAccount({int balance = 0}) : super(balance: balance);
-
-  @override
-  void withdraw(int amount) {
-    if (amount > balance) {
-      print('Noto\'g\'ri amal! Hisobingiz yetarli emas.');
-    } else {
-      super.withdraw(amount);
-    }
-  }
-}
-
-void main() {
-  SavingsAccount savingsAccount = SavingsAccount(balance: 1000);
-  savingsAccount.deposit(500);
-  savingsAccount.withdraw(2000);
-  savingsAccount.withdraw(200);
-
-  CheckingAccount checkingAccount = CheckingAccount(balance: 2000);
-  checkingAccount.deposit(1000);
-  checkingAccount.withdraw(2500);
-  checkingAccount.withdraw(200);
 }
